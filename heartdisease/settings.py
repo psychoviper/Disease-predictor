ee6d2d81ab271de86a28633dfcc23331fea722fe
psychoviper <popsufi16@gmail.com>
2022-12-11 18:13:18 +0530

    merge

--- heartdisease/settings.py
+++ heartdisease/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd1v#2-xne%^%s!_&-k0m#3awh+u2$3zx(jyiie+9^9fs3$^bbl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 EMAIL_USE_TLS = True
